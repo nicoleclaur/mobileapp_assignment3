f115ecb6f3b49df7c0a56033c6239e662d47eb5a
BryanTeruel <164897126+BryanTeruel@users.noreply.github.com>
2025-03-25 09:12:09 -0600

    Create signUp.tsx

---
+++ forms/app/signUp.tsx
@@ -0,0 +1,173 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const signInSchema = Yup.object().shape({
+  firstName: Yup.string().required("First Name is required"),
+  lastName: Yup.string().required("Last Name is required"),
+  email: Yup.string().email("Invalid Email").required("Email is required"),
+  password: Yup.string().min(6, "Too short").required("Password is required"),
+  confirmPassword: Yup.string()
+    .min(6, "Too short")
+    .required("Password is required")
+    .oneOf([Yup.ref("password")], "Passwords must match"),
+  phoneNumber: Yup.string()
+    .matches(/^[0-9]+$/, "Must be only digits")
+    .min(10, "Phone number must be at least 10")
+    .required("Phone Number is required"),
+});
+
+const signup = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign up </Text>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+          phoneNumber: "",
+        }}
+        validationSchema={signInSchema}
+        onSubmit={(handleSubmit) => {
+          console.log(handleSubmit);
+        }}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="First Name"
+              onChangeText={handleChange("firstName")}
+              onBlur={handleBlur("firstName")}
+              value={values.firstName}
+            />
+
+            {errors.firstName && (
+              <Text style={styles.error}>{errors.firstName}</Text>
+            )}
+
+            <TextInput
+              style={styles.input}
+              placeholder="Last Name"
+              onChangeText={handleChange("lastName")}
+              onBlur={handleBlur("lastName")}
+              value={values.lastName}
+            />
+
+            {errors.lastName && (
+              <Text style={styles.error}>{errors.lastName}</Text>
+            )}
+
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+
+            {errors.email && <Text style={styles.error}>{errors.email}</Text>}
+
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+
+            {errors.password && (
+              <Text style={styles.error}>{errors.password}</Text>
+            )}
+
+            <TextInput
+              style={styles.input}
+              placeholder="Confirm password"
+              secureTextEntry
+              onChangeText={handleChange("confirmPassword")}
+              onBlur={handleBlur("confirmPassword")}
+              value={values.confirmPassword}
+            />
+
+            {errors.confirmPassword && (
+              <Text style={styles.error}>{errors.confirmPassword}</Text>
+            )}
+
+            <TextInput
+              style={styles.input}
+              placeholder="Phone Number"
+              onChangeText={handleChange("phoneNumber")}
+              onBlur={handleBlur("phoneNumber")}
+              value={values.phoneNumber}
+            />
+
+            {errors.phoneNumber && (
+              <Text style={styles.error}>{errors.phoneNumber}</Text>
+            )}
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => handleSubmit()}
+            >
+              <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+export default signup;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#F5F5F5",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: "15%",
+    marginBottom: 20,
+  },
+  input: {
+    width: 300,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "#CCCCCC",
+    marginBottom: 10,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 8,
+  },
+  error: {
+    color: "red",
+    fontSize: 12,
+    marginVertical: 5,
+    marginLeft: 5,
+  },
+  button: {
+    backgroundColor: "#007aff",
+    padding: 12,
+    borderRadius: 8,
+    width: 300,
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
