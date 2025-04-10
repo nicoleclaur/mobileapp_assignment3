09e1d658c583e8aa5fa36d48ec3bbd2d42ce6371
Mitzi Vera <144722779+mitzi-vera@users.noreply.github.com>
2025-04-09 21:54:11 -0600

    models/user.js

--- forms-backend/models/user.js
+++ forms-backend/models/user.js
@@ -1,1 +1,13 @@
 // models/user.js
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phoneNumber: { type: String },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
